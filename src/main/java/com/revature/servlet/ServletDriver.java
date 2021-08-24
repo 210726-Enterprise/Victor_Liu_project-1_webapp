43864997c4f8a29218f795fdf564a227a40954eb
cptvictor <victorsyanwenliu19@gmail.com>
2021-08-24 02:30:55 -0400

    getting database info from properties file

--- src/main/java/com/revature/servlet/ServletDriver.java
+++ src/main/java/com/revature/servlet/ServletDriver.java
@@ -5,27 +5,48 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.revature.model.VideoGame;
 import com.revature.orm.util.ORM;
 import com.revature.service.VideoGameService;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 @WebListener
 public class ServletDriver implements ServletContextListener
 {
+    private static final Logger logger = LoggerFactory.getLogger(VideoGameService.class);
+
     @Override
     public void contextInitialized(ServletContextEvent sce)
     {
         List<Class> classList = new ArrayList<>();
         classList.add(VideoGame.class);
+
+        Properties properties = new Properties();
+
+        try
+        {
+            FileReader propertyReader = new FileReader(ServletDriver.class.getClassLoader()
+                    .getResource("application.properties").getPath()
+                    .replaceAll("%20", " "));
+            properties.load(propertyReader);
+        }
+        catch (IOException e)
+        {
+            logger.warn(e.getMessage(), e);
+        }
         ORM videoGameORM = new ORM(
-                "jdbc:postgresql://revature-assignment-db.c4c3no36zu7c.us-east-2.rds.amazonaws.com:5432/postgres",
-                "postgres",
-                "rI3VK00VlEcazaJ8XXoE",
+                properties.getProperty("url"),
+                properties.getProperty("username"),
+                properties.getProperty("password"),
                 classList);
+
         ObjectMapper objectMapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
 
         VideoGameService videoGameService = new VideoGameService(videoGameORM, objectMapper);
